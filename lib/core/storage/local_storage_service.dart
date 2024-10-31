ece53921f979f5ecdbde74b309b2be50012dca54
TuranAktass <turan.ktas@gmail.com>
2024-10-31 16:27:30 +0300

    hive initialization added

--- lib/core/storage/local_storage_service.dart
+++ lib/core/storage/local_storage_service.dart
@@ -1,6 +1,7 @@
 abstract class LocalStorageService {
-
   Future<dynamic> save(String key, dynamic value);
   Future<dynamic> delete(String key);
   Future<dynamic> get(String key);
+
+  Future<dynamic> initBox();
 }
--- lib/core/storage/local_storage_service_impl.dart
+++ lib/core/storage/local_storage_service_impl.dart
@@ -1,13 +1,27 @@
 import 'package:fcore/core/storage/local_storage_service.dart';
-import 'package:hive/hive.dart';
+import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class LocalStorageServiceImpl implements LocalStorageService {
   late Box _storageBox;
 
+  LocalStorageServiceImpl({required this.boxName});
+
+  final String boxName;
+
+  @override
+  Future initBox() async {
+    if (!kIsWeb && !Hive.isBoxOpen(boxName)) {
+      await Hive.initFlutter();
+    }
+
+    return await Hive.openBox(boxName);
+  }
+
   @override
   Future delete(String key) async {
     try {
-      _storageBox = await Hive.openBox("gelirim");
+      _storageBox = await Hive.openBox(boxName);
       return _storageBox.delete(key);
     } catch (e) {
       rethrow;
@@ -16,13 +30,13 @@ class LocalStorageServiceImpl implements LocalStorageService {
 
   @override
   Future get(String key) async {
-    _storageBox = await Hive.openBox("gelirim");
+    _storageBox = await Hive.openBox(boxName);
     return _storageBox.get(key);
   }
 
   @override
   Future save(String key, dynamic value) async {
-    _storageBox = await Hive.openBox("gelirim");
+    _storageBox = await Hive.openBox(boxName);
     return _storageBox.put(key, value);
   }
 }
